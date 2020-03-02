804a5562fa12c6f07942b36c2ae81bbb28018d3b
Avijit Bairagi <ovi821@gmail.com>
2020-03-03 01:32:40 +0600

    account module created

---
+++ src/main/java/com/mrrobot/overflow/account/model/Role.java
@@ -0,0 +1,20 @@
+package com.mrrobot.overflow.account.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+    Boolean isActive;
+}

8ea6c2e82b054b1ae895b74f5b0b99e17e92fb81
Avijit Bairagi <ovi821@gmail.com>
2020-03-08 01:46:15 +0600

    notfound and alreday exits exception added

---
+++ src/main/java/com/mrrobot/overflow/common/exception/NotFoundException.java
@@ -0,0 +1,29 @@
+package com.mrrobot.overflow.common.exception;
+
+public class NotFoundException extends Exception{
+    private String code;
+    private String message;
+
+    public NotFoundException(String code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

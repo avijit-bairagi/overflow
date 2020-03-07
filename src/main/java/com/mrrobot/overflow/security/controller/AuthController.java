906b4c902d7f1141965f7b1ccb7f7f33306be74b
Avijit Bairagi <ovi821@gmail.com>
2020-03-07 18:13:26 +0600

    response data updated for auth controller

--- src/main/java/com/mrrobot/overflow/security/controller/AuthController.java
+++ src/main/java/com/mrrobot/overflow/security/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.mrrobot.overflow.security.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mrrobot.overflow.common.model.Response;
 import com.mrrobot.overflow.common.utils.ResponseStatus;
+import com.mrrobot.overflow.profile.model.Profile;
 import com.mrrobot.overflow.profile.model.Role;
 import com.mrrobot.overflow.profile.model.User;
+import com.mrrobot.overflow.profile.service.ProfileService;
 import com.mrrobot.overflow.profile.service.RoleService;
 import com.mrrobot.overflow.profile.service.UserService;
 import com.mrrobot.overflow.security.jwt.JwtProvider;
@@ -14,6 +15,7 @@ import com.mrrobot.overflow.security.model.RegistrationBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,6 +27,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -40,6 +43,9 @@ public class AuthController {
     UserService userService;
 
     @Autowired
+    ProfileService profileService;
+
+    @Autowired
     RoleService roleService;
 
     @Autowired
@@ -48,24 +54,19 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     Logger log = LoggerFactory.getLogger("debug-logger");
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginBody loginBody) {
 
+        log.debug("authenticateUser(): start");
+
         Response response = new Response();
 
         try {
             Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginBody.getUsername(),
-                            loginBody.getPassword()
-                    )
+                    new UsernamePasswordAuthenticationToken(loginBody.getUsername(), loginBody.getPassword())
             );
-
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
@@ -74,8 +75,6 @@ public class AuthController {
             response.setCode(ResponseStatus.SUCCESS.value());
             response.setMessage("Login successful!");
             response.setData(new JwtResponse(jwt));
-
-            return ResponseEntity.badRequest().body(response);
 
         } catch (BadCredentialsException e) {
             log.error("errorMessage={}", e.getMessage());
@@ -87,50 +86,78 @@ public class AuthController {
             response.setMessage(e.getMessage());
         }
 
-        return ResponseEntity.badRequest().body(response);
+        log.debug("authenticateUser(): end");
+
+        if (response.getCode().equalsIgnoreCase(ResponseStatus.SUCCESS.value()))
+            return ResponseEntity.ok().body(response);
+        else
+            return ResponseEntity.badRequest().body(response);
     }
 
     @PostMapping("/register")
     public ResponseEntity<Response> registerUser(@Valid @RequestBody RegistrationBody registrationBody) {
 
+        log.debug("registerUser(): start");
+
         Response response = new Response();
 
-        if (userService.existsByUsername(registrationBody.getUsername())) {
+        try {
+            if (userService.existsByUsername(registrationBody.getUsername())) {
 
-            response.setCode(ResponseStatus.ALREADY_EXITS.value());
-            response.setMessage("Username is already taken!");
+                response.setCode(ResponseStatus.ALREADY_EXITS.value());
+                response.setMessage("Username is already taken!");
 
-            return ResponseEntity.badRequest().body(response);
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            if (userService.existsByEmail(registrationBody.getEmail())) {
+
+                response.setCode(ResponseStatus.ALREADY_EXITS.value());
+                response.setMessage("Email is already taken!");
+
+                return ResponseEntity.badRequest().body(response);
+            }
+
+            User user = new User(registrationBody.getFirstName(), registrationBody.getUsername(),
+                    registrationBody.getEmail(), encoder.encode(registrationBody.getPassword()));
+
+            Set<String> strRoles = registrationBody.getRole();
+            Set<Role> roles = new HashSet<>();
+
+            strRoles.forEach(r -> {
+
+                Role role = roleService.findByName("ROLE_" + r)
+                        .orElseThrow(() -> new RuntimeException(r + " role not found!"));
+                roles.add(role);
+            });
+
+            user.setRoles(roles);
+            User userData = userService.save(user);
+
+            Profile profile = new Profile(registrationBody.getFirstName(), registrationBody.getLastName(),
+                    registrationBody.getAddressLine(), registrationBody.getCity(), registrationBody.getPhoneNo(), registrationBody.getIsOpenForJob());
+            profile.setCreatedDate(new Date());
+            profile.setLevel(0);
+            profile.setPoint(0d);
+            profile.setUserId(userData.getId());
+
+            profileService.save(profile);
+
+            response.setCode(ResponseStatus.SUCCESS.value());
+            response.setMessage("User registered successfully!");
+            response.setData(userData);
+
+        } catch (RuntimeException e) {
+            log.error("errorMessage={}", e.getMessage());
+            response.setCode(ResponseStatus.NOT_FOUND.value());
+            response.setMessage(e.getMessage());
         }
 
-        if (userService.existsByEmail(registrationBody.getEmail())) {
+        log.debug("registerUser(): end");
 
-            response.setCode(ResponseStatus.ALREADY_EXITS.value());
-            response.setMessage("Email is already taken!");
-
+        if (response.getCode().equalsIgnoreCase(ResponseStatus.SUCCESS.value()))
+            return ResponseEntity.ok().body(response);
+        else
             return ResponseEntity.badRequest().body(response);
-        }
-
-        User user = new User(registrationBody.getName(), registrationBody.getUsername(),
-                registrationBody.getEmail(), encoder.encode(registrationBody.getPassword()));
-
-        Set<String> strRoles = registrationBody.getRole();
-        Set<Role> roles = new HashSet<>();
-
-        strRoles.forEach(r -> {
-
-            Role role = roleService.findByName("ROLE_" + r)
-                    .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-            roles.add(role);
-        });
-
-        user.setRoles(roles);
-        User data = userService.save(user);
-
-        response.setCode(ResponseStatus.SUCCESS.value());
-        response.setMessage("User registered successfully!");
-        response.setData(data);
-
-        return ResponseEntity.ok().body(response);
     }
 }

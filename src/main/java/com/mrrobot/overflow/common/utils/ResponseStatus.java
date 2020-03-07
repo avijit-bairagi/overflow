4abe2cb372af502d2d44a273e05c29db42af4415
Avijit Bairagi <ovi821@gmail.com>
2020-03-07 13:46:25 +0600

    login response code added

--- src/main/java/com/mrrobot/overflow/common/utils/ResponseStatus.java
+++ src/main/java/com/mrrobot/overflow/common/utils/ResponseStatus.java
@@ -3,7 +3,9 @@ package com.mrrobot.overflow.common.utils;
 public enum ResponseStatus {
 
     SUCCESS("1000", "Success"),
-    ALREADY_EXITS("1404", "Already Exits")  ;
+    ALREADY_EXITS("1404", "Already Exits"),
+    BAD_CREDENTIALS("1401", "Bad credentials"),
+    AUTH_ERROR("1111", "Auth error");
 
     private final String value;
     private final String description;

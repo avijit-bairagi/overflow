603a46849e39d2f0520a63bf004948cb68b2908d
Avijit Bairagi <ovi821@gmail.com>
2020-03-11 02:48:24 +0600

    getPostById method added

--- src/main/java/com/mrrobot/overflow/post/service/PostServiceImpl.java
+++ src/main/java/com/mrrobot/overflow/post/service/PostServiceImpl.java
@@ -9,12 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostServiceImpl implements PostService{
 
     @Autowired
     PostRepository postRepository;
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return postRepository.findById(id);
+    }
 
     @Override
     public List<Post> findAll() {

d7868fa35744ca17e5c90ae79ff78553984cd953
Avijit Bairagi <ovi821@gmail.com>
2020-03-11 02:44:05 +0600

    new model added

---
+++ src/main/java/com/mrrobot/overflow/post/model/CommentBody.java
@@ -0,0 +1,12 @@
+package com.mrrobot.overflow.post.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentBody {
+
+    String description;
+    Long commentedBy;
+}
---
+++ src/main/java/com/mrrobot/overflow/post/model/PostResponse.java
@@ -0,0 +1,28 @@
+package com.mrrobot.overflow.post.model;
+
+import com.mrrobot.overflow.post.entity.Comment;
+import com.mrrobot.overflow.post.entity.Topic;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+public class PostResponse {
+
+    String title;
+    String description;
+    Integer hit;
+    Integer favourite;
+    Double point;
+    Long postedBy;
+    Long groupId;
+    Date createdDate;
+    Date updatedDate;
+
+    Set<Topic> topics;
+    List<Comment> comments;
+}

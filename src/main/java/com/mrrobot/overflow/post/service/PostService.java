c47f6085cded115be847f4babd0a8db4ac0322f6
Avijit Bairagi <ovi821@gmail.com>
2020-03-11 02:52:08 +0600

    getPostById method added

--- src/main/java/com/mrrobot/overflow/post/service/PostService.java
+++ src/main/java/com/mrrobot/overflow/post/service/PostService.java
@@ -5,8 +5,12 @@ import com.mrrobot.overflow.common.exception.NotFoundException;
 import com.mrrobot.overflow.post.entity.Post;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
+
+    Optional<Post> findById(Long id);
+
     List<Post> findAll();
 
     Post save(Post post) throws AlreadyExitsException;

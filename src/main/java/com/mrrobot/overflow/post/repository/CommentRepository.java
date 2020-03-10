87df8c10c1d1184b9d9b7c597c9bd401f8c4b553
Avijit Bairagi <ovi821@gmail.com>
2020-03-11 02:45:25 +0600

    comment repository added

---
+++ src/main/java/com/mrrobot/overflow/post/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.mrrobot.overflow.post.repository;
+
+import com.mrrobot.overflow.post.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findByDescription(String description);
+    List<Comment> findByPostId(Long postId);
+}

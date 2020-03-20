e64d534c4aa6818d97275b78949f49f4b006330f
Avijit Bairagi <ovi821@gmail.com>
2020-03-21 03:57:35 +0600

    post ranking updated

--- src/main/java/com/mrrobot/overflow/common/utils/UserLevel.java
+++ src/main/java/com/mrrobot/overflow/common/utils/UserLevel.java
@@ -2,22 +2,23 @@ package com.mrrobot.overflow.common.utils;
 
 public enum UserLevel {
 
-    BEGINNER(1, "BEGINNER", 100, 1.0),
-    MASTER(2, "MASTER", 1000, 1.5),
-    PRO(3, "PRO", 2000, 1.8);
-
+    BEGINNER(1, "BEGINNER", 100, 1.0, 0.1),
+    MASTER(2, "MASTER", 1000, 1.5, 0.5),
+    PRO(3, "PRO", 2000, 1.8, 0.8);
 
     private final Integer level;
     private final String name;
     private final Integer levelPoint;
-    private final Double value;
+    private final Double positiveValue;
+    private final Double negativeValue;
 
 
-    UserLevel(int level, String name, int levelPoint, Double value) {
+    UserLevel(int level, String name, int levelPoint, Double positiveValue, Double negativeValue) {
         this.level = level;
         this.name = name;
         this.levelPoint = levelPoint;
-        this.value = value;
+        this.positiveValue = positiveValue;
+        this.negativeValue = negativeValue;
     }
 
     public static UserLevel findByLevel(Integer level) {
@@ -41,7 +42,11 @@ public enum UserLevel {
         return levelPoint;
     }
 
-    public Double getValue() {
-        return value;
+    public Double getPositiveValue() {
+        return positiveValue;
+    }
+
+    public Double getNegativeValue() {
+        return negativeValue;
     }
 }
--- src/main/java/com/mrrobot/overflow/rank/RankServiceImpl.java
+++ src/main/java/com/mrrobot/overflow/rank/RankServiceImpl.java
@@ -4,14 +4,18 @@ import com.mrrobot.overflow.common.utils.UserLevel;
 import com.mrrobot.overflow.profile.entity.Profile;
 import com.mrrobot.overflow.profile.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Configuration
 @Service
 public class RankServiceImpl implements RankService {
 
-    private final Double defaultPoint = 5.0;
+    @Value("${post.rank.defaultPoint}")
+    private Double defaultPoint;
 
     @Autowired
     ProfileService profileService;
@@ -19,18 +23,14 @@ public class RankServiceImpl implements RankService {
     @Override
     public Double getPoint(Long userId, boolean isUpVote) {
 
-        Double point = 0.0;
-
         Optional<Profile> profileOptional = profileService.findByUserId(userId);
 
         if (profileOptional.isEmpty())
-            point = defaultPoint;
-        else
-            point = (double) UserLevel.findByLevel(profileOptional.get().getLevel()).getLevel() * defaultPoint;
+            return defaultPoint;
 
         if(isUpVote)
-            return point;
+            return UserLevel.findByLevel(profileOptional.get().getLevel()).getPositiveValue() * defaultPoint;
         else
-            return -(point/2);
+            return -UserLevel.findByLevel(profileOptional.get().getLevel()).getNegativeValue() * defaultPoint;
     }
 }

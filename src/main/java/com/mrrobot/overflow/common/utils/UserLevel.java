6f0d7edf6777a52f4c1c0b12c9d7d7167ac51192
Avijit Bairagi <ovi821@gmail.com>
2020-03-23 02:55:54 +0600

    get level by point added

--- src/main/java/com/mrrobot/overflow/common/utils/UserLevel.java
+++ src/main/java/com/mrrobot/overflow/common/utils/UserLevel.java
@@ -1,10 +1,15 @@
 package com.mrrobot.overflow.common.utils;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public enum UserLevel {
 
     BEGINNER(1, "BEGINNER", 100, 1.0, 0.1),
     MASTER(2, "MASTER", 1000, 1.5, 0.5),
-    PRO(3, "PRO", 2000, 1.8, 0.8);
+    PRO(4, "PRO", 2000, 1.8, 0.8),
+    EXPERT(3, "EXPERT", 1500, 1.7, 0.6);
 
     private final Integer level;
     private final String name;
@@ -30,6 +35,23 @@ public enum UserLevel {
         return BEGINNER;
     }
 
+    public static Integer getLevelByPoint(Double point) {
+
+        AtomicInteger level = new AtomicInteger(1);
+
+        SortedMap<Integer, UserLevel> map = new TreeMap<>();
+        for (UserLevel l : UserLevel.values()) {
+            map.put(l.getLevel(), l);
+        }
+
+        map.forEach((integer, userLevel) -> {
+            if (userLevel.getLevelPoint() > point)
+                return;
+            level.set(userLevel.getLevel());
+        });
+        return level.get();
+    }
+
     public int getLevel() {
         return level;
     }
@@ -49,4 +71,5 @@ public enum UserLevel {
     public Double getNegativeValue() {
         return negativeValue;
     }
+
 }

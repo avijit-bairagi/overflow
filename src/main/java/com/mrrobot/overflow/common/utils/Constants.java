107aa4d9051734c92aefb715b6e5a77b9e785f19
avijit-bkash <avijit.i2gether@bkash.com>
2020-08-26 15:24:19 +0600

    user role added to constant

--- src/main/java/com/mrrobot/overflow/common/utils/Constants.java
+++ src/main/java/com/mrrobot/overflow/common/utils/Constants.java
@@ -3,4 +3,5 @@ package com.mrrobot.overflow.common.utils;
 public class Constants {
 
     public static final String AUTH_HEADER = "Authorization";
+    public static final String USER_ROLE = "ROLE_USER";
 }

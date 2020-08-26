5e998a76f31a4fb11d6bb8c53139a83b22f1397c
avijit-bkash <avijit.i2gether@bkash.com>
2020-08-26 15:27:19 +0600

    roles removed from registration body

--- src/main/java/com/mrrobot/overflow/profile/model/RegistrationBody.java
+++ src/main/java/com/mrrobot/overflow/profile/model/RegistrationBody.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -20,8 +19,6 @@ public class RegistrationBody {
     @Size(max = 60)
     @Email
     private String email;
-
-    private Set<String> role;
 
     @NotBlank
     @Size(min = 6, max = 40)
